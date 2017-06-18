9bcb84c6e7e0e20d1850caf2f88cf976336089f0
yang xiaotian <yangxiaotian1995@outlook.com>
2017-06-18 11:59:25 +0800

    server of tcp without thread!

---
+++ server.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int main()
+{
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	//char ip[] = "123.206.214.192";
+	if(sockfd < 0)
+	{
+		printf("creat socket error!\n");
+		return -1;
+	}
+
+	struct sockaddr_in addr;
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	//addr.sin_addr.s_addr = htonl(ip);
+	//addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons(9999);
+	
+	int ret = bind(sockfd, (struct sockaddr*)&addr, sizeof(struct sockaddr_in));
+	if(ret < 0)
+	{
+		printf("bind error!\n");
+		return -1;
+	}
+	
+	listen(sockfd, 100);
+
+	int accfd;
+	//while(1)
+	{
+		struct sockaddr_in clientAddr;
+		socklen_t len = sizeof(clientAddr);
+		accfd = accept(sockfd, (struct sockaddr*)&clientAddr, &len);
+		if(accfd < 0)
+		{
+			printf("accept error!\n");
+			return -1;
+		}
+	}
+	while (1)
+	{
+	
+		char buf[128];
+		int rlen = recv(accfd, buf, sizeof(buf), 0);
+		if(rlen < 0)
+		{
+			printf("message receive error!\n");
+			return -1;
+		}
+		else if(rlen > 0)
+			printf("receive message from client: %s\n", buf);
+	}
+	close(accfd);
+	close(sockfd);
+	return 0;
+}
